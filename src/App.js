cdef7184c163ac20be76ca0a81deb52f5a603779
tgiangk1 <giang.ttt@primecommerce.edu.vn>
2025-05-21 11:55:45 +0700

    fix: remove nested router to fix routing issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import BannerSlider from './components/BannerSlider';
 import MovieSection from './components/MovieSection';
